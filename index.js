e1fb7479190f7287ccaefbd388a44d4b0f99590f
Francesco Lupo <fra.lupo.91@icloud.com>
2021-04-14 17:18:15 +0200

    - fix 3

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.use(
   })
 );
 
+app.listen(process.env.PORT || 5000);
+
 const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@sandbox-shard-00-00.dnjvp.mongodb.net:27017,sandbox-shard-00-01.dnjvp.mongodb.net:27017,sandbox-shard-00-02.dnjvp.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-yivg7u-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose
@@ -30,5 +32,3 @@ mongoose
   .catch((error) => {
     throw error;
   });
-
-app.listen(process.env.PORT || 5000);
