21f036c6d8a972a5d0c12604dd9ad5c408e13c6c
Francesco Lupo <fra.lupo.91@icloud.com>
2021-04-14 18:04:23 +0200

    - fix

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(process.env.PORT || 5000);
 app.use(
   "/graphql",
   cors(),
-  graphqlHttp(() => ({
+  graphqlHTTP(() => ({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true,
