1bf1cc18b8ee9bb1ce7a1f3de9783e7f2675f396
Francesco Lupo <fra.lupo.91@icloud.com>
2021-04-14 17:19:31 +0200

    - fix 4

--- index.js
+++ index.js
@@ -10,19 +10,18 @@ app.get("/", (req, res) => {
   res.send(`Hello World!`);
 });
 
+app.listen(process.env.PORT || 5000);
+
 app.use(cors());
 
 app.use(
   "/graphql",
-  bodyParser.json(),
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true,
   })
 );
-
-app.listen(process.env.PORT || 5000);
 
 const uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@sandbox-shard-00-00.dnjvp.mongodb.net:27017,sandbox-shard-00-01.dnjvp.mongodb.net:27017,sandbox-shard-00-02.dnjvp.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-yivg7u-shard-0&authSource=admin&retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
